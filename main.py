ffbd77c84bf60ff1325aa5b56823f19b25f41a98
allabergenovvyacheslav <slawushkin@mail.ru>
2024-06-07 17:51:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print(len('The first president of Russia'))
+first = 4
+second = 2
+print(first + second)
+print(first - second)
+first = 4
+second = 2
+third = 6
+print((first+second+third)//3)
+first_string = 'вторник'
+second_string = 'понедельник, '
+print(second_string, first_string)
+a = 4
+b = 2
+c = 6
+f = (a*b)+(a*c)
+print((f **3)/2)
